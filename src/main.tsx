c4e5bdcc3b22f5f3e28b006750339d75fa8ca564
yerimi00 <fhddl1019@naver.com>
2025-07-10 10:12:03 +0900

    [feat]글로벌 css 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { PATHS } from './constants/paths.ts';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Main from './components/page/Main.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { PATHS } from "./constants/paths.ts";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./components/page/Main.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       {
-        path: PATHS.MAIN,
+        index: true,
         element: <Main />,
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
