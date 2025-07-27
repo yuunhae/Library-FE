458df8b424441f13f7b75891d88e016f9039c645
leestana01 <leestana01@naver.com>
2025-07-27 17:24:30 +0900

    feat: 카카오맵 타입 정의

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,25 @@
 /// <reference types="vite/client" />
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+declare namespace kakao.maps {
+  class Map {
+    constructor(container: HTMLElement, options: any);
+    setCenter(latlng: LatLng): void;
+  }
+  
+  class LatLng {
+    constructor(lat: number, lng: number);
+  }
+  
+  class Marker {
+    constructor(options: any);
+    setMap(map: Map | null): void;
+  }
+  
+  function load(callback: () => void): void;
+}
